00f84be23dd8bbd0f524f200908c446924c80e66
Matias <matiasmicheletto@gmail.com>
2019-04-02 21:41:39 -0300

    Fotos de perfil almacenadas en storage

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,183 +1,240 @@
 app.controller("profile", ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
 
-  if (!$rootScope.userLogged) {
-    $location.path("/login");
-    return;
-  }
-
-
-  // Callback al subir foto de perfil
-  document.getElementById("imgInput").addEventListener('change',
-    function () {
-      var file = document.getElementById("imgInput").files[0];
-      var reader = new FileReader();
-      reader.onloadend = function () {
-        $scope.newAvatar = reader.result;
-        $scope.$apply();
-      }
-      if (file) reader.readAsDataURL(file);
-    });
-
-  $scope.getUserNames = function (userUids) { // Devuelve los apellidos de los usuarios cuyos uid se pasa como arreglo
-    if ($scope.users) { // Esperar a que se bajen de la db
-      var names = [];
-      for (var k in userUids)
-        names.push($scope.users[userUids[k]].secondName);
-      return names.join(); // Apellidos separados por coma
+    if (!$rootScope.userLogged) {
+        $location.path("/login");
+        return;
     }
-  };
 
-  $scope.uploadPic = function () { // Redirigir el evento al input para cargar una foto
-    document.getElementById("imgInput").click();
-  };
+    document.getElementById("imgInput").addEventListener('change', // Callback al subir foto de perfil
+        function () { // Cuando sube una nueva foto, agregar a la vista como base64
+            $rootScope.loading = true;
+            $scope.$apply();
+            var file = document.getElementById("imgInput").files[0];
+            var reader = new FileReader();
+            reader.onloadend = function () {
+                $scope.newAvatar = reader.result; // Se muestra temporalmente esta imagen
+                $scope.avatarFile = file; // Guardo el archivo para subir luego a storage
+                $scope.newProfilePic = true;
+                $rootScope.loading = false;
+                $scope.$apply();
+            }
+            if (file) reader.readAsDataURL(file);
+        });
 
-  $scope.saveForm = function () { // Guardar cambios en db
+    $scope.getUserNames = function (userUids) { // Devuelve los apellidos de los usuarios cuyos uid se pasa como arreglo
+        if ($scope.users) { // Esperar a que se bajen de la db
+            var names = [];
+            for (var k in userUids)
+                names.push($scope.users[userUids[k]].secondName);
+            return names.join(); // Apellidos separados por coma
+        }
+    };
+
+    $scope.uploadPic = function () { // Redirigir el evento al input para cargar una foto
+        document.getElementById("imgInput").click();
+    };
+
+    $scope.saveForm = function () { // Guardar cambios en db
+        $rootScope.loading = true;
+
+        // Valores de los formularios
+        var newData = {
+            name: document.getElementById("inputName").value,
+            secondName: document.getElementById("inputSecondName").value,
+            lu: document.getElementById("inputLU").value,
+            degree: document.getElementById("inputDegree").value,
+            partners: [] // Arreglo de companeros de comision, se completa luego
+        }
+
+        // Validar datos de formulario
+        if (newData.name == "" || newData.secondName == "" || newData.lu == "" || newData.degree == "") {
+            console.log("Uno de los campos queda vacio.");
+            M.toast({
+                html: "Complete todos los campos",
+                classes: 'rounded red',
+                displayLength: 2500
+            });
+            $rootScope.loading = false;
+            return;
+        }
+
+        // Buscar companeros de comision ingresados y generar arreglo de uids
+        var getUserUID = function (name) { // Obtener el uid a partir del nombre completo
+            for (var k in $scope.users)
+                if ($scope.users[k].name + " " + $scope.users[k].secondName == name) // Coincidencia
+                    return k;
+        };
+
+        // Validar los nombres de companeros de comision (deben existir usuarios, no pueden ser iguales)
+        if (document.getElementById("partner1").value != "") {
+            var partUid = getUserUID(document.getElementById("partner1").value);
+            if (partUid)
+                newData.partners.push(partUid);
+            else {
+                M.toast({
+                    html: "Nombre de usuario no válido",
+                    classes: 'rounded red',
+                    displayLength: 2500
+                });
+                $rootScope.loading = false;
+                return;
+            }
+        }
+        if (document.getElementById("partner2").value != "") {
+            var partUid = getUserUID(document.getElementById("partner2").value);
+            if (partUid)
+                newData.partners.push(partUid);
+            else {
+                M.toast({
+                    html: "Nombre de usuario no válido",
+                    classes: 'rounded red',
+                    displayLength: 2500
+                });
+                $rootScope.loading = false;
+                return;
+            }
+        }
+        if (newData.partners.length == 2) { // En caso de ingresar dos compañeros
+            if (newData.partners[0] == newData.partners[1]) { // No pueden ser iguales
+                M.toast({
+                    html: "Nombres de compañeros iguales",
+                    classes: 'rounded red',
+                    displayLength: 2500
+                });
+                $rootScope.loading = false;
+                return;
+            }
+        }
+        // Falta verificar que ninguno de los dos companeros ingresado sean iguales al usuario actual
+
+        // Habria que incluir referencia al usuario actual en los companeros indicados
+        var updateNewData = function(){ // Actualizar entrada en la db
+            Cipressus.db.update(newData, "users_public/" + $rootScope.user.uid) // Actualizar
+                .then(function (res) {
+                    M.toast({
+                        html: "Datos actualizados!",
+                        classes: 'rounded green darken-3',
+                        displayLength: 2500
+                    });
+                    // Actualizar datos de view
+                    $rootScope.user.name = newData.name;
+                    $rootScope.user.secondName = newData.secondName;
+                    $rootScope.user.lu = newData.lu;
+                    $rootScope.user.degree = newData.degree;
+                    $rootScope.user.partners = newData.partners;
+                    $rootScope.loading = false;
+                    $scope.edit = false;
+                    $scope.$apply();
+                })
+                .catch(function (err) {
+                    console.log(err);
+                    M.toast({
+                        html: "Ocurrió un problema al actualizar los datos",
+                        classes: 'rounded red',
+                        displayLength: 2500
+                    });
+                    $rootScope.loading = false;
+                    $scope.$apply();
+                });
+        }
+
+        if ($scope.newProfilePic) { // Si puso nueva foto
+            var saveNewAvatar = function () { // Guardar nueva foto de perfil en storage
+                var filename = Cipressus.utils.generateFileName(25) + "." + $scope.avatarFile.name.split(".")[1]; // Generar nombre de archivo
+                Cipressus.storage.put($scope.avatarFile, "Images", filename) // Guardar imagen en storage
+                    .then(function (res) {                        
+                        // Datos que van al storage
+                        newData.avatar = res.url;
+                        newData.avatarFilename = filename;
+                        // Actualizar datos de la vista
+                        $rootScope.user.avatar = res.url; // Enlace a la imagen
+                        $rootScope.user.avatarFilename = filename; // Nombre de archivo en storage
+                        updateNewData(); // Finalmente, actualizar todos los datos
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                        M.toast({
+                            html: "Ocurrió un error al actualizar imagen",
+                            classes: 'rounded red',
+                            displayLength: 2500
+                        });
+                        $rootScope.loading = false;
+                        $scope.$apply();
+                    });
+            }
+            // Si ya tenia foto de perfil, eliminar la anterior
+            if ($rootScope.user.avatarFilename) {
+                Cipressus.storage.delete($rootScope.user.avatarFilename, "Images")
+                    .then(function (res) {
+                        saveNewAvatar(); // Luego de borrar, guardar la nueva
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                        M.toast({
+                            html: "Ocurrió un error al actualizar imagen",
+                            classes: 'rounded red',
+                            displayLength: 2500
+                        });
+                        saveNewAvatar(); // Intentar cargar nueva de todas maneras
+                    });
+            }else{ // Si no tenia foto de perfil, cargar directamente
+                saveNewAvatar();
+            }
+        }else{ // Si no cambio la foto de perfil, actualizar solo los datos
+            updateNewData();
+        }
+    };
+
+
+    ///// Inicialización controller
     $rootScope.loading = true;
+    $rootScope.sidenav.close();
 
-    // Valores de los formularios
-    var name = document.getElementById("inputName").value;
-    var secondName = document.getElementById("inputSecondName").value;
-    var lu = document.getElementById("inputLU").value;
-    var degree = document.getElementById("inputDegree").value;
-    // Nueva foto de perfil
-    $rootScope.user.avatar = $scope.newAvatar;
+    $scope.edit = false; // Toggle para la edicion de los datos del perfil
+    $scope.newProfilePic = false; // Indicador para saber si hay que actualizar foto de perfil
+    $scope.created = $rootScope.user.enrolled > 0 ? moment($rootScope.user.enrolled).format("DD/MM/YYYY HH:mm") : "(Aún no aprobado)"; // Formato legible
+    $scope.newAvatar = $rootScope.user.avatar; // Nuevo src si carga nueva foto de perfil
 
-    // Validar inputs y actualizar 
-    if (name != "") $rootScope.user.name = name;
-    if (secondName != "") $rootScope.user.secondName = secondName;
-    if (lu != "") $rootScope.user.lu = lu;
-    if (degree != "") $rootScope.user.degree = degree;
+    M.Tooltip.init(document.querySelectorAll('.tooltipped'), {}); // Inicializar tooltips      
 
-    // Buscar companeros de comision
-    var getUserUID = function (name) { // Obtener el uid a partir del nombre completo
-      for (var k in $scope.users)
-        if ($scope.users[k].name + " " + $scope.users[k].secondName == name) // Coincidencia
-          return k;
-    };
-    var partners = [];
-    if (document.getElementById("partner1").value != "") {
-      var partUid = getUserUID(document.getElementById("partner1").value);
-      if (partUid)
-        partners.push(partUid);
-      else {
-        M.toast({
-          html: "Nombre de usuario no válido",
-          classes: 'rounded red',
-          displayLength: 2500
-        });
-        $rootScope.loading = false;
-        return;
-      }
-    }
-    if (document.getElementById("partner2").value != "") {
-      var partUid = getUserUID(document.getElementById("partner2").value);
-      if (partUid)
-        partners.push(partUid);
-      else {
-        M.toast({
-          html: "Nombre de usuario no válido",
-          classes: 'rounded red',
-          displayLength: 2500
-        });
-        $rootScope.loading = false;
-        return;
-      }
-    }
-
-    if(partners.length == 2){ // En caso de ingresar dos compañeros
-      if(partners[0] == partners[1]){ // No pueden ser iguales
-        M.toast({
-          html: "Nombres de compañeros iguales",
-          classes: 'rounded red',
-          displayLength: 2500
-        });
-        $rootScope.loading = false;
-        return;
-      }
-    }
-
-    $rootScope.user.partners = partners;
-
-    var newData = { // Objeto para subir a la db
-      name: $rootScope.user.name,
-      secondName: $rootScope.user.secondName,
-      lu: $rootScope.user.lu,
-      degree: $rootScope.user.degree,
-      avatar: $rootScope.user.avatar,
-      partners: partners
-    };
-
-    // Faltaria incluir en los otros usuarios la referencia a este
-    Cipressus.db.update(newData, "users_public/" + $rootScope.user.uid) // Actualizar
-      .then(function (res) {
-        M.toast({
-          html: "Datos actualizados!",
-          classes: 'rounded green darken-3',
-          displayLength: 2500
-        });
-        $rootScope.loading = false;
-        $scope.edit = false;
-        $scope.$apply();
-      })
-      .catch(function (err) {
-        console.log(err);
-        M.toast({
-          html: "Ocurrió un problema al actualizar los datos",
-          classes: 'rounded red',
-          displayLength: 2500
-        });
-        $rootScope.loading = false;
-        $scope.$apply();
-      });
-  };
-
-
-  ///// Inicialización controller
-  $rootScope.loading = true;
-  $rootScope.sidenav.close();
-
-  $scope.edit = false; // Toggle para la edicion de los datos del perfil
-  $scope.created = $rootScope.user.enrolled > 0 ? moment($rootScope.user.enrolled).format("DD/MM/YYYY HH:mm") : "(Aún no aprobado)"; // Formato legible
-  $scope.newAvatar = $rootScope.user.avatar; // Nuevo src si carga nueva foto de perfil
-
-  M.Tooltip.init(document.querySelectorAll('.tooltipped'), {}); // Inicializar tooltips      
-
-  Cipressus.utils.activityCntr($rootScope.user.uid, "profile").catch(function (err) {console.log(err)});
-
-  Cipressus.db.get("users_public") // Descargar datos de usuarios
-    .then(function (users_public_data) {
-      $scope.users = users_public_data;
-      // Lista de usuarios para autocompletador
-      var userList = {};
-      for (var k in $scope.users) // Para excluir admins hay que bajar users_private
-        userList[$scope.users[k].name + " " + $scope.users[k].secondName] = $scope.users[k].avatar; // Lista de nombres y fotos
-      M.Autocomplete.init(document.querySelectorAll('.autocomplete'), {
-        data: userList
-      });
-
-      // Inicializar inputs
-      document.getElementById("inputName").value = $rootScope.user.name;
-      document.getElementById("inputSecondName").value = $rootScope.user.secondName;
-      document.getElementById("inputLU").value = $rootScope.user.lu;
-      document.getElementById("inputDegree").value = $rootScope.user.degree;
-      if ($rootScope.user.partners) {
-        if ($rootScope.user.partners[0])
-          document.getElementById("partner1").value = $scope.users[$rootScope.user.partners[0]].name + " " + $scope.users[$rootScope.user.partners[0]].secondName;
-        if ($rootScope.user.partners[1])
-          document.getElementById("partner2").value = $scope.users[$rootScope.user.partners[1]].name + " " + $scope.users[$rootScope.user.partners[1]].secondName;
-      }
-
-      M.FormSelect.init(document.querySelectorAll('select'), {});
-      M.updateTextFields(); // Para mostrar los placeholders
-      $rootScope.loading = false;
-      $scope.$apply();
-    })
-    .catch(function (err) {
-      console.log(err);
-      M.toast({
-        html: "Ocurrió un error al acceder a la base de datos",
-        classes: 'rounded red',
-        displayLength: 2000
-      });
+    Cipressus.utils.activityCntr($rootScope.user.uid, "profile").catch(function (err) {
+        console.log(err)
     });
+
+    Cipressus.db.get("users_public") // Descargar datos de usuarios
+        .then(function (users_public_data) {
+            $scope.users = users_public_data;
+            // Lista de usuarios para autocompletador
+            var userList = {};
+            for (var k in $scope.users) // Para excluir admins hay que bajar users_private
+                userList[$scope.users[k].name + " " + $scope.users[k].secondName] = $scope.users[k].avatar; // Lista de nombres y fotos
+            M.Autocomplete.init(document.querySelectorAll('.autocomplete'), {
+                data: userList
+            });
+
+            // Inicializar inputs
+            document.getElementById("inputName").value = $rootScope.user.name;
+            document.getElementById("inputSecondName").value = $rootScope.user.secondName;
+            document.getElementById("inputLU").value = $rootScope.user.lu;
+            document.getElementById("inputDegree").value = $rootScope.user.degree;
+            if ($rootScope.user.partners) {
+                if ($rootScope.user.partners[0])
+                    document.getElementById("partner1").value = $scope.users[$rootScope.user.partners[0]].name + " " + $scope.users[$rootScope.user.partners[0]].secondName;
+                if ($rootScope.user.partners[1])
+                    document.getElementById("partner2").value = $scope.users[$rootScope.user.partners[1]].name + " " + $scope.users[$rootScope.user.partners[1]].secondName;
+            }
+
+            M.FormSelect.init(document.querySelectorAll('select'), {});
+            M.updateTextFields(); // Para mostrar los placeholders
+            $rootScope.loading = false;
+            $scope.$apply();
+        })
+        .catch(function (err) {
+            console.log(err);
+            M.toast({
+                html: "Ocurrió un error al acceder a la base de datos",
+                classes: 'rounded red',
+                displayLength: 2000
+            });
+        });
 }]);
