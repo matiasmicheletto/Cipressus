8be2506249da47ff073c65fd26266ccf7cba97e7
Matias <matiasmicheletto@gmail.com>
2019-04-07 11:11:15 -0300

    En simulador, guardar solo componentes y conexiones en vez del modelo completo

--- controllers/simulator.js
+++ controllers/simulator.js
@@ -38,8 +38,48 @@ app.controller("simulator", ['$scope', '$rootScope', '$location', function ($sco
     };
 
     $scope.loadCircuit = function(key){ // Cargar circuito
-        var data = $scope.simulations[key].data;
-        simcir.setupSimcir($('#simcir'), JSON.parse(data));
+        var data = JSON.parse($scope.simulations[key].data);
+        // Poner barra de herramientas
+        data.showToolbox = true;
+        data.width = document.getElementById("simcir").clientWidth;
+        data.height = document.getElementById("simcir").clientHeight
+        data.toolbox = [
+          {"type":"In"},
+          {"type":"Out"},
+          {"type":"Joint"},
+          {"type":"DC"},
+          {"type":"LED"},
+          {"type":"PushOff"},
+          {"type":"PushOn"},
+          {"type":"Toggle"},
+          {"type":"BUF"},
+          {"type":"NOT"},
+          {"type":"AND"},
+          {"type":"NAND"},
+          {"type":"OR"},
+          {"type":"NOR"},
+          {"type":"XOR"},
+          {"type":"XNOR"},
+          {"type":"OSC"},
+          {"type":"7seg"},
+          {"type":"16seg"},
+          {"type":"4bit7seg"},
+          {"type":"RotaryEncoder"},
+          {"type":"BusIn"},
+          {"type":"BusOut"},
+          {"type":"RS-FF"},
+          {"type":"JK-FF"},
+          {"type":"T-FF"},
+          {"type":"D-FF"},
+          {"type":"8bitCounter"},
+          {"type":"HalfAdder"},
+          {"type":"FullAdder"},
+          {"type":"4bitAdder"},
+          {"type":"2to4BinaryDecoder"},
+          {"type":"3to8BinaryDecoder"},
+          {"type":"4to16BinaryDecoder"}
+        ];
+        simcir.setupSimcir($('#simcir'), data);
         currentSim = {
             key: key,
             name: $scope.simulations[key].name
@@ -62,9 +102,8 @@ app.controller("simulator", ['$scope', '$rootScope', '$location', function ($sco
                     };
 
         // Crear objeto a guardar en db
-        var workspace = simcir.controller($('#simcir').find('.simcir-workspace'));
-        var data = workspace.text(); 
-        var model = workspace.data();
+        var model = simcir.controller($('#simcir').find('.simcir-workspace')).data(); // Objeto con datos de simulacion
+        var data = JSON.stringify((({ devices, connectors }) => ({ devices, connectors }))(model)); // Solo guardar dos propiedades
 
         if(model.devices.length == 0){ // No guardar si no hay componentes
             M.toast({html: "No hay componentes!",classes: 'rounded red',displayLength: 2000});
@@ -73,7 +112,7 @@ app.controller("simulator", ['$scope', '$rootScope', '$location', function ($sco
             var sim = {
                 timestamp: Date.now(),
                 size: model.devices.length,
-                data: data,
+                data: data, 
                 name: $scope.circuitFileName
             };
     
@@ -125,9 +164,10 @@ app.controller("simulator", ['$scope', '$rootScope', '$location', function ($sco
     var save_modal = M.Modal.init(document.getElementById("save_modal"), {preventScrolling: false});
 
     // Inicializar simulador
-    var divHeight = document.getElementById("simcir").clientHeight;
-    var divWidth = document.getElementById("simcir").clientWidth;
-    simcir.setupSimcir($('#simcir'), {width: divWidth,height: divHeight});
+    simcir.setupSimcir($('#simcir'), {
+        width: document.getElementById("simcir").clientWidth,
+        height: document.getElementById("simcir").clientHeight
+    });
 
     Cipressus.utils.activityCntr($rootScope.user.uid, "simulator").catch(function (err) {console.log(err)});
 }]);
